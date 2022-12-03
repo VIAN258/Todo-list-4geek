da9975333a6e87032569b946b267772526c245b4
VIAN258 <angelmorales891@gmail.com>
2022-12-03 15:18:13 +0000

    actuaizacio1

--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -5,34 +5,34 @@ import React from "react";
 
 //create your first component
 const Input = () => {
+  const [task, setTask] = React.useState("");
+  const [newTask, setNewTask] = React.useState(['']);
 
-        const [input, setInput ] = React.useState({
-               tareas:''
+  const validate = (e) => {
+    if (e.key === "Enter" && e.target.value !== "") {
+        console.log(e.target.value);
+        setNewTask(newTask.concat(e.target.value));
+     // setNewTask((prev) => [...prev, e.target.value]);
+      console.log(newTask);
+    }
+  };
+  //const handleInputChange = (e) => {
+  // setTask(e.target.value)
+  //console.log(task)
 
-      });
-
-        const handleInputChange = (e) => {
-                console.log(e.target.value)
-                setInput({
-                        ...input,
-                        [e.target.name] :  e.target.name
-                })
-
-        }
-                
-	return (
-        
-            <input 
-            type="text"
-            placeholder="Ingrese tarea"
-            clasName= "form-control"
-            name="tareas"
-            onChange={handleInputChange}
-            onKeyDown={e =>handleInputChange(e)}
-            ></input>
-            
-	);
+  // }
+  return (
+    <input
+      type="text"
+      placeholder="Ingrese tarea"
+      clasName="form-control"
+      onChange={validate}
+      onKeyDown={(e) => validate(e)}
+      value
+    ></input>
+  );
 };
 
 export default Input;
 
+//crear funcion validador de input si se ingresa dato o no, usar mappara recorrido en la cre<CION DE LISTA
--- src/js/component/lists.jsx
+++ src/js/component/lists.jsx
@@ -5,6 +5,7 @@ import React from "react";
 
 
 
+
 //create your first component
 const Lists = () => {
 	return (
