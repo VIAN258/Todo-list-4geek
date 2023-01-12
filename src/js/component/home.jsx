481ba6b1be92158077c084d3f1ce3fa2f6fdaeaa
VIAN258 <angelmorales891@gmail.com>
2023-01-12 20:27:12 +0000

    finish

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,43 +5,52 @@ import { useState } from "react";
 //create your first component
 
 
-
-let counter =0;
 const Home = () => {
-
-	const [newTask, setNewTask] = useState([])
-	const validate = (e) => {
-		if(e.key ==='Enter'){
-			setNewTask(prev => [...prev, e.target.value])
-			counter++
-		}	
-	}
-
-
-const deleteTask = (newTas) => {
-	 setNewTask (newTask.filter(p => p !=newTas))
-	 counter --
+    const [newTask, setNewTask] = useState([])
+	// Se agregó el estado para el valor del input
+	const [taskValue, setTaskValue] = useState("");
+    const validate = (e) => {
+        if(e.key ==='Enter'){
+			// Se cambió el valor que se le da a la tarea por el valor del estado
+            setNewTask(current => [...current, taskValue])
+            e.target.value = "";
+			setTaskValue("")
+        }
+    }
+const deleteTask = (index) => {
+	//Obtenemos el índice a traves del botón de eliminar, eliminamos ese espacio del array
+	//y retornamos el nuevo array sin ese elemento
+	 setNewTask(current =>[
+        ...current.slice(0, index),
+        ...current.slice(index + 1, current.length)
+        ]);
 }
   return (
-	<div className="container">
-		<div className="card">
-		<div className="card-body">
-			<h5> Task List.</h5>
-			<input  type="text" className="form-control" placeholder="Enter task" onKeyDown={e => validate(e)}/> 
-			<p></p>
-			<ul className="list-group" >
-				<li id="first" className="list-group-item"> Added tasks:  {counter<1? "No Tasks, add task": counter}</li><p></p>
+    <div className="container">
+        <div className="card">
+        <div className="card-body">
+            <h5> Task List.</h5>
+			{/* Se agregaron los atributos value y onChange para que guarde 
+			el valor del input en un estado cada vez que este cambie */}
+            <input  type="text" value={taskValue} className="form-control" placeholder="Enter task" onChange={(e) => setTaskValue(e.target.value)} onKeyDown={(e) => validate(e)}/>
+            <ul className="list-group" >
+				{/* Se eliminó la variable counter y se cambió por el largo del array, 
+				ya que esto cuenta automáticamente la cantidad de espacios que tiene. */}
+                <li id="first" className="list-group-item"> Added tasks:  {newTask.length<1? "No Tasks, add task": newTask.length}</li><p></p>
+            </ul>
+			<ul className="list-group">
+			{/* Aquí se modificó el map para que tomara el índice 
+			donde se encuentro el elemento de la lista para así mostrarlo y poder eliminarlo */}
+            {newTask && newTask?.map ((item, index) => {
+                return <li key={index} onClick ={() => deleteTask(index)}  className="list-group-item">{item}  <button  type="button" className="btn-close" aria-label="Close"></button></li>
+            })
+            }
 			</ul>
-			{newTask && newTask.map ((newTas) => {
-				return <ul className="list-group">
-							<li key={newTas.toString()} onClick ={() => deleteTask(newTas.toString())}  className="list-group-item">{newTas}  <button  type="button" class="btn-close" aria-label="Close"></button></li><p></p>
-						</ul>	  
-			})
-			}
-		</div>
-		</div>
-	</div>
+        </div>
+        </div>
+    </div>
   );
 };
 
 export default Home;
+
