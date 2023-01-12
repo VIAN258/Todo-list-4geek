83ec3de7dff3b5e89ae35b0eddf5fdff7be09b6d
VIAN258 <angelmorales891@gmail.com>
2023-01-12 16:07:01 +0000

    close hided

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -14,29 +14,32 @@ const Home = () => {
 		if(e.key ==='Enter'){
 			setNewTask(prev => [...prev, e.target.value])
 			counter++
-		}
+		}	
 	}
+
 
 const deleteTask = (newTas) => {
 	 setNewTask (newTask.filter(p => p !=newTas))
 	 counter --
 }
   return (
-    <div className="card">
-      <div className="card-body">
-		<h5> Task List.</h5>
-		<input type="text" className="form-control" placeholder="Enter task" onKeyDown={e => validate(e)}/> 
-		<ul className="list-group">
-			<li className="list-group-item">Added tasks:  {counter<1? "No Task": counter}</li>
-		</ul>
-		{newTask && newTask.map ((newTas) => {
-              return <ul className="list-group">
-			               <li key={newTas.toString()} onClick ={() => deleteTask(newTas.toString())}  className="list-group-item">{newTas}  <button type="button" class="btn-close" aria-label="Close"></button></li>
-					 </ul>	  
-		})
-		}
-	  </div>
-    </div>
+	<div className="container">
+		<div className="card">
+		<div className="card-body">
+			<h5> Task List.</h5>
+			<input  type="text" className="form-control" placeholder="Enter task" onKeyDown={e => validate(e)}/> 
+			<ul className="list-group">
+				<li className="list-group-item">Added tasks:  {counter<1? "No Tasks, add task": counter}</li>
+			</ul>
+			{newTask && newTask.map ((newTas) => {
+				return <ul className="list-group">
+							<li key={newTas.toString()} onClick ={() => deleteTask(newTas.toString())}  className="list-group-item">{newTas}  <button  type="button" class="btn-close" aria-label="Close"></button></li>
+						</ul>	  
+			})
+			}
+		</div>
+		</div>
+	</div>
   );
 };
 
